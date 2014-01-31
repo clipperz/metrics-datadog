be632c3eb1f0237d1b33f41514ee10e5ac27d6b3
Giulio Cesare Solaroli <giulio.cesare@solaroli.it>
2014-01-31 19:13:03 +0100

    Added a 'ping' method

--- src/main/java/com/codahale/metrics/reporting/HttpTransport.java
+++ src/main/java/com/codahale/metrics/reporting/HttpTransport.java
@@ -15,69 +15,94 @@ import com.ning.http.client.HttpResponseHeaders;
 import com.ning.http.client.HttpResponseStatus;
 
 public class HttpTransport implements Transport {
-  private final String apiKey;
-  private final AsyncHttpClient client;
-  private final String seriesUrl;
-  private static final Logger LOG = LoggerFactory
-      .getLogger(HttpTransport.class);
 
-  public HttpTransport(String host, String apiKey) {
-    this.apiKey = apiKey;
-    this.client = new AsyncHttpClient();
-    this.seriesUrl = String.format("https://%s/api/v1/series?api_key=%s", host,
-        apiKey);
-  }
+	private final String apiKey;
+	private final AsyncHttpClient client;
+	private final String host;
+	private final String seriesUrl;
+	private static final Logger LOG = LoggerFactory.getLogger(HttpTransport.class);
 
-  public static class HttpRequest implements Transport.Request {
-    private final BoundRequestBuilder requestBuilder;
-    private final ByteArrayOutputStream out;
+	public HttpTransport(String host, String apiKey) {
+		this.host = host;
+		this.apiKey = apiKey;
+		this.client = new AsyncHttpClient();
+		this.seriesUrl = String.format("https://%s/api/v1/series?api_key=%s", host, apiKey);
+	}
 
-    public HttpRequest(HttpTransport transport, String apiKey,
-        BoundRequestBuilder requestBuilder) throws IOException {
-      this.requestBuilder = requestBuilder;
-      this.requestBuilder.addHeader("Content-Type", "application/json");
-      this.out = new ByteArrayOutputStream();
-    }
+	public static class HttpRequest implements Transport.Request {
 
-    public OutputStream getBodyWriter() {
-      return out;
-    }
+		private final BoundRequestBuilder requestBuilder;
+		private final ByteArrayOutputStream out;
 
-    public void send() throws Exception {
-      out.flush();
-      out.close();
-      requestBuilder.setBody(out.toByteArray())
-          .execute(new AsyncHandler<Void>() {
+		public HttpRequest(HttpTransport transport, String apiKey, BoundRequestBuilder requestBuilder) throws IOException {
+			this.requestBuilder = requestBuilder;
+			this.requestBuilder.addHeader("Content-Type", "application/json");
+			this.out = new ByteArrayOutputStream();
+		}
 
-            public STATE onBodyPartReceived(HttpResponseBodyPart bp)
-                throws Exception {
-              return STATE.CONTINUE;
-            }
+		public OutputStream getBodyWriter() {
+			return out;
+		}
 
-            public Void onCompleted() throws Exception {
-              return null;
-            }
+		public void send() throws Exception {
+			out.flush();
+			out.close();
+			requestBuilder.setBody(out.toByteArray()).execute(new AsyncHandler<Void>() {
+				public STATE onBodyPartReceived(HttpResponseBodyPart bp) throws Exception {
+					return STATE.CONTINUE;
+				}
 
-            public STATE onHeadersReceived(HttpResponseHeaders headers)
-                throws Exception {
-              return STATE.CONTINUE;
-            }
+				public Void onCompleted() throws Exception {
+					return null;
+				}
 
-            public STATE onStatusReceived(HttpResponseStatus arg0)
-                throws Exception {
-              return STATE.CONTINUE;
-            }
+				public STATE onHeadersReceived(HttpResponseHeaders headers) throws Exception {
+					return STATE.CONTINUE;
+				}
 
-            public void onThrowable(Throwable t) {
-              LOG.error("Error Writing Datadog metrics", t);
-            }
+				public STATE onStatusReceived(HttpResponseStatus arg0) throws Exception {
+					return STATE.CONTINUE;
+				}
 
-          }).get();
-    }
-  }
+				public void onThrowable(Throwable t) {
+					LOG.error("Error Writing Datadog metrics", t);
+				}
+			}).get();
+		}
+	}
 
-  public HttpRequest prepare() throws IOException {
-    BoundRequestBuilder builder = client.preparePost(seriesUrl);
-    return new HttpRequest(this, apiKey, builder);
-  }
+	public HttpRequest prepare() throws IOException {
+		BoundRequestBuilder builder = client.preparePost(seriesUrl);
+		return new HttpRequest(this, apiKey, builder);
+	}
+
+	public void ping() {
+		String	url = String.format("https://%s/api/", "app.datadoghq.com");
+		BoundRequestBuilder builder = client.prepareGet(seriesUrl);
+		try {
+			builder.execute(new AsyncHandler<Void>() {
+				public STATE onBodyPartReceived(HttpResponseBodyPart bp) throws Exception {
+					return STATE.CONTINUE;
+				}
+
+				public Void onCompleted() throws Exception {
+					return null;
+				}
+
+				public STATE onHeadersReceived(HttpResponseHeaders headers) throws Exception {
+					return STATE.CONTINUE;
+				}
+
+				public STATE onStatusReceived(HttpResponseStatus arg0) throws Exception {
+					return STATE.CONTINUE;
+				}
+
+				public void onThrowable(Throwable t) {
+					LOG.error("Failed ping", t);
+				}
+			}).get();
+		} catch (Exception exception) {
+			LOG.error("Failed to ping target server", exception);
+		}
+	}
 }
